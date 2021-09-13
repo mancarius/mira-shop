093aad45ab7ffd7392643972ec3999d0c4b174f6
Mattia <75211269+mancarius@users.noreply.github.com>
2021-09-13 19:50:39 +0200

    new

---
+++ src/test/MockShipment.ts
@@ -0,0 +1,9 @@
+import { ShippingType } from "src/app/shared/interfaces/shipping-type";
+
+export const MockShipment: ShippingType = {
+    delivery_days: 2,
+    is_available: true,
+    price: 0,
+    name: 'Regular',
+    id: 'fake-shipment-id'
+}
