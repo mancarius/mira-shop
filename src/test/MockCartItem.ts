47078d839a64969b5c68a810632d3d38ceedd2f0
Mattia <75211269+mancarius@users.noreply.github.com>
2021-09-13 19:50:12 +0200

    fixed 'createdAt' attribute type

--- src/test/MockCartItem.ts
+++ src/test/MockCartItem.ts
@@ -1,10 +1,10 @@
 import { CartItem } from 'src/app/shared/interfaces/cart-item';
-import { MockProduct } from './MockProduct';
+import firebase from 'firebase';
 
 export const MockCartItem: CartItem = {
-    createdAt: new Date,
-    amount: 2,
-    product: {
-        id: 'FakeProductID',
-    }
-}
+  createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
+  amount: 2,
+  product: {
+    id: 'FakeProductID',
+  },
+};
