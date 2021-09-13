2a4a9a2219ea407c48922a9ce4c6de65fab3aca6
Mattia <75211269+mancarius@users.noreply.github.com>
2021-09-13 19:49:26 +0200

    added getShipmentById method

--- src/app/services/shipment.service.ts
+++ src/app/services/shipment.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {
   AngularFirestore,
   AngularFirestoreCollection,
+  CollectionReference,
 } from '@angular/fire/firestore';
 import firebase from 'firebase';
 import { ShippingType } from '../shared/interfaces/shipping-type';
@@ -10,18 +11,18 @@ import { ShippingType } from '../shared/interfaces/shipping-type';
   providedIn: 'root',
 })
 export class ShipmentService {
-  private shipmentsCollection: AngularFirestoreCollection<ShippingType>;
-
+  private shipmentsCollectionRef: CollectionReference<ShippingType>;
   public favoriteShipmentType: ShippingType | undefined;
 
   constructor(private afs: AngularFirestore) {
-    this.shipmentsCollection = this.afs.collection<ShippingType>('shipments');
+    this.shipmentsCollectionRef =
+      this.afs.collection<ShippingType>('shipments').ref;
   }
 
   public async getAvailableShippingTypes(): Promise<ShippingType[]> {
     let query: firebase.firestore.QuerySnapshot<ShippingType>;
     try {
-      query = await this.shipmentsCollection.ref
+      query = await this.shipmentsCollectionRef
         .where('is_available', '==', true)
         .get();
     } catch (err: any) {
@@ -33,5 +34,16 @@ export class ShipmentService {
         return { ...doc.data(), id: doc.id };
       }
     );
+  }
+
+  public async getShipmentById(
+    id: ShippingType['id']
+  ): Promise<ShippingType | null> {
+    const doc = await this.shipmentsCollectionRef.doc(id).get();
+    if (doc?.id) {
+      return { ...doc.data(), id: doc.id } as ShippingType;
+    } else {
+      return null;
+    }
   }
 }
