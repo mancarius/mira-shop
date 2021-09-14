79f7adc21ce421fa7ae5fd9206d2b53669b89ad7
Mattia <75211269+mancarius@users.noreply.github.com>
2021-09-14 14:46:28 +0200

    success error handling test for #getAvailableShippingTypes and #getShipmentById

--- src/app/services/shipment.service.spec.ts
+++ src/app/services/shipment.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestoreStub } from 'src/test/AngularFirestoreStub/AngularFirestoreStub';
+import { ErrorHandlerService } from './error-handler.service';
 import { ShipmentService } from './shipment.service';
 
 describe('ShipmentService', () => {
   let service: ShipmentService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: AngularFirestore, useClass: AngularFirestoreStub },
+        { provide: ErrorHandlerService, useValue: { add: () => {} } },
+      ],
+    });
     service = TestBed.inject(ShipmentService);
   });
 
@@ -14,7 +21,47 @@ describe('ShipmentService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('#all should return all shipment types', () => {});
+  it('#getAvailableShippingTypes should return a rejection when #_shipmentsCollectionRef is undefined', async () => {
+    service['_shipmentsCollectionRef'] = undefined;
 
-  it('#get should return the given shipment type', () => {});
+    expectAsync(service.getAvailableShippingTypes()).toBeRejectedWith(
+      'Unable to load shipping types'
+    );
+  });
+
+  it('#getAvailableShippingTypes should return a rejection when fail', async () => {
+    const fakeErrorMessage = 'Some error happened';
+    spyOn<any>(service['_shipmentsCollectionRef'], 'where').and.throwError(
+      fakeErrorMessage
+    );
+
+    expectAsync(service.getAvailableShippingTypes()).toBeRejectedWith(
+      fakeErrorMessage
+    );
+  });
+
+  it('#getShipmentById should return a rejection when #_shipmentsCollectionRef is undefined', async () => {
+    service['_shipmentsCollectionRef'] = undefined;
+
+    expectAsync(service.getShipmentById('fake-id')).toBeRejectedWith(
+      'Unable to load shipping types'
+    );
+  });
+
+  it('#getShipmentById should return a rejection when fail', async () => {
+    const fakeErrorMessage = 'Some error happened';
+    spyOn<any>(service['_shipmentsCollectionRef'], 'doc').and.throwError(
+      fakeErrorMessage
+    );
+
+    expectAsync(service.getShipmentById('fake_id')).toBeRejectedWith(
+      fakeErrorMessage
+    );
+  });
+
+  it('#getShipmentById should return a rejection when an invalid param is passed', async () => {
+    expectAsync(service.getShipmentById('')).toBeRejectedWith(
+      'The shipment id received is not a valid string.'
+    );
+  });
 });
