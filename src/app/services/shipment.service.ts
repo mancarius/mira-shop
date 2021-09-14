6b587a65f527c5c0ff5287c96ee40a5e5b990b43
Mattia <75211269+mancarius@users.noreply.github.com>
2021-09-14 14:44:55 +0200

    added error handler for collection request and some check inside methods

--- src/app/services/shipment.service.ts
+++ src/app/services/shipment.service.ts
@@ -1,37 +1,51 @@
 import { Injectable } from '@angular/core';
-import {
-  AngularFirestore,
-  AngularFirestoreCollection,
-  CollectionReference,
-} from '@angular/fire/firestore';
+import { AngularFirestore, CollectionReference } from '@angular/fire/firestore';
 import firebase from 'firebase';
 import { ShippingType } from '../shared/interfaces/shipping-type';
+import { ErrorHandlerService } from './error-handler.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShipmentService {
-  private shipmentsCollectionRef: CollectionReference<ShippingType>;
+  private _shipmentsCollectionRef:
+    | CollectionReference<ShippingType>
+    | undefined;
   public favoriteShipmentType: ShippingType | undefined;
 
-  constructor(private afs: AngularFirestore) {
-    this.shipmentsCollectionRef =
-      this.afs.collection<ShippingType>('shipments').ref;
+  constructor(private _afs: AngularFirestore, private _errorHandler: ErrorHandlerService) {
+    try {
+      this._shipmentsCollectionRef =
+        this._afs.collection<ShippingType>('shipments').ref;
+    } catch (err: any) {
+      _errorHandler.add(err);
+      console.error(err);
+    }
   }
 
+  /**
+   * Return a promise that resolves to an array of available shipping types
+   *
+   * @returns
+   */
   public async getAvailableShippingTypes(): Promise<ShippingType[]> {
+    if (!this._shipmentsCollectionRef) {
+      throw new Error('Unable to load shipping types');
+    }
+
     let query: firebase.firestore.QuerySnapshot<ShippingType>;
+
     try {
-      query = await this.shipmentsCollectionRef
+      query = await this._shipmentsCollectionRef
         .where('is_available', '==', true)
         .get();
     } catch (err: any) {
-      return Promise.reject(err.message ?? err);
+      return Promise.reject(err);
     }
 
     return query.docs.map(
       (doc: firebase.firestore.QueryDocumentSnapshot<ShippingType>) => {
-        return { ...doc.data(), id: doc.id };
+        return { ...doc?.data(), id: doc?.id };
       }
     );
   }
@@ -39,8 +53,21 @@ export class ShipmentService {
   public async getShipmentById(
     id: ShippingType['id']
   ): Promise<ShippingType | null> {
-    const doc = await this.shipmentsCollectionRef.doc(id).get();
-    if (doc?.id) {
+    if (!this._shipmentsCollectionRef) {
+      throw new Error('Unable to load shipping types');
+    }
+
+    if (typeof id !== 'string' || id.length == 0) {
+      throw new Error('The shipment id received is not a valid string.');
+    }
+    let doc: firebase.firestore.DocumentSnapshot<ShippingType>;
+    try {
+      doc = await this._shipmentsCollectionRef.doc(id).get();
+    } catch (err: any) {
+      throw err;
+    }
+
+    if (doc) {
       return { ...doc.data(), id: doc.id } as ShippingType;
     } else {
       return null;
